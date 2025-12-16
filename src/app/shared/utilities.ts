d4216318575be1ae51e993650813159638584469
Ryan Luttrull <reluttrull@gmail.com>
2025-12-16 10:16:41 -0600

    set up dev and prod environments and cli deployment to gh pages

--- src/app/shared/utilities.ts
+++ src/app/shared/utilities.ts
@@ -1,5 +1,7 @@
+import { environment } from '../../environments/environment';
+
 export class utilities {
-  public static serverUrl = 'https://localhost:7019';
+  public static serverUrl = environment.serverUrl;
   public static getPath(isMultiplayer: boolean): string {
     return isMultiplayer ? 'multiplayer' : 'singleplayer';
   }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  serverUrl: 'https://senetserver-bta5duhybyfrc0f3.canadacentral-01.azurewebsites.net/'
+};
